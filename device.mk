be11820c81ad89c4827dc13a75fa781dbbf6d60d
zclkkk <2484392714@qq.com>
2022-09-10 15:03:01 +0000

    alioth: Drop lights AIDL
    Signed-off-by: zclkkk <2484392714@qq.com>

--- device.mk
+++ device.mk
@@ -260,10 +260,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm

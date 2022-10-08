37e226c7f03ebd18225216dc58d248c5bac77545
zclkkk <zclkkk@aliyun.com>
2022-10-08 17:35:10 +0000

    alioth: Build GNSS stack from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,10 @@ TARGET_USES_ION := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 4.0
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/vintf/framework_compatibility_matrix.xml \

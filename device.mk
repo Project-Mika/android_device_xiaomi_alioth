9ec36bb2c9170ffa3f8853a4e652b95874c17f4d
zclkkk <zclkkk@aliyun.com>
2022-10-14 20:25:33 +0800

    alioth: Setup VAB snapshot compression  feature

--- device.mk
+++ device.mk
@@ -55,7 +55,7 @@ PRODUCT_COPY_FILES += \
 
 # A/B
 # Inherit virtual_ab_ota product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
